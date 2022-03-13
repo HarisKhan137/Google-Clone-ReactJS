14a68136965e002ecd956dfb8f316ac477c35993
Muhammad Haris Khan <mhkhan.bscs19seecs@seecs.edu.pk>
2022-03-13 21:36:54 +0500

    Components Skeleton Made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React,   {useState} from 'react'
 
-function App() {
+import { Navbar } from './components/Navbar'
+import { Footer } from './components/Footer'
+import { Routes } from './components/Routes'
+
+const App = () => {
+    const [darkTheme,setDarkTheme] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ={darkTheme ? 'dark' : ''}>
+      
+        <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+                <h1>App </h1>
+            <Navbar></Navbar>
+            <Routes></Routes>
+            <Footer></Footer>
+
+
+
+
+        </div>
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
